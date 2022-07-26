aec26f2125b86b23f6eb9ecbf86efc9a7fdbe3f2
haroldnc <harold.mth95@gmail.com>
2022-07-26 02:08:20 -0500

    Agregamos la ruta iecho al servidor

--- api/src/server.js
+++ api/src/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const iecho = require("./routes/iecho");
 
 const server = express();
 
 server.use(cors());
+
+server.use("/iecho", iecho);
 
 module.exports = server;
