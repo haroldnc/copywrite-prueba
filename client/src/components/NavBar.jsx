97556998d80220b87462f9d13107dab285c8bbdc
haroldnc <harold.mth95@gmail.com>
2022-07-27 01:02:58 -0500

    Agregamos la funcionalidad al NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { newText } from "../redux/actions";
 import { Button, Form, Navbar } from "react-bootstrap";
 import "./css/NavBar.css";
 
 export default function NavBar() {
+    const dispatch = useDispatch();
+
+    const onClick = (e) => {
+        e.preventDefault();
+
+        const search_input = document.getElementById("search-input");
+
+        if (!search_input.value) search_input.focus();
+        else {
+            dispatch(newText(search_input.value));
+            search_input.value = "";
+        }
+    };
+
     console.log("render: NavBar");
 
     return (
@@ -13,8 +29,13 @@ export default function NavBar() {
                     placeholder="Insert Text"
                     className="me-2"
                     aria-label="Search"
+                    id="search-input"
                 />
-                <Button variant="primary" className="btn-send">
+                <Button
+                    variant="primary"
+                    className="btn-send"
+                    onClick={onClick}
+                >
                     Send
                 </Button>
             </Form>
