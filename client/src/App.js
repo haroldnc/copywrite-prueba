00c11d70a01594e464273c91ad94796e86b0264d
haroldnc <harold.mth95@gmail.com>
2022-07-27 00:37:06 -0500

    Acoplamos redux al proyecto

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
 import CardResult from "./components/CardResult";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
     console.log("render: App");
 
     return (
-        <>
+        <Provider store={store}>
             <NavBar />
             <CardResult />
-        </>
+        </Provider>
     );
 }
 
