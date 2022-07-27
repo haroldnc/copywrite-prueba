d6d942ec73c1c6525d1ad64c6ff35deb004c3095
haroldnc <harold.mth95@gmail.com>
2022-07-26 22:53:23 -0500

    Acomplamos en App el componente NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello World</h1>
-        </div>
+        <>
+            <NavBar />
+        </>
     );
 }
 
