3f15e95fbdbb7587cc1c2a4d1ede8f59a46eb446
haroldnc <harold.mth95@gmail.com>
2022-07-26 23:18:20 -0500

    Corregimos los estilos para ser responsive

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Button, Form, Navbar } from "react-bootstrap";
+import "./css/NavBar.css";
 
 export default function NavBar() {
+    console.log("render: NavBar");
+
     return (
         <Navbar bg="danger" expand="lg">
-            <Form className="d-flex mx-auto">
+            <Form className="d-flex mx-auto search-bar">
                 <Form.Control
                     type="search"
-                    placeholder="Search"
+                    placeholder="Insert Text"
                     className="me-2"
                     aria-label="Search"
                 />
-                <Button variant="primary">Search</Button>
+                <Button variant="primary" className="btn-send">
+                    Send
+                </Button>
             </Form>
         </Navbar>
     );
