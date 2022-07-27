f0b5383972ae115536da93fa81a907919e615057
haroldnc <harold.mth95@gmail.com>
2022-07-26 23:22:02 -0500

    Creamos el componente CardResult

---
+++ client/src/components/CardResult.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Card, ListGroup } from "react-bootstrap";
+
+export default function CardResult() {
+    console.log("render: CardResult");
+    const items = [
+        "Doceavo Item",
+        "Onceavo Item",
+        "Decimo Item",
+        "Noveno Item",
+        "Octavo Item",
+        "Septimo Item",
+        "Sexto Item",
+        "Quinto Item",
+        "Cuarto Item",
+        "Tercer Item",
+        "Segundo Item",
+        "Primer Item",
+    ];
+
+    return (
+        <Container className="col-lg-8">
+            <Card style={{ width: "100%" }} className="mt-5">
+                <Card.Body>
+                    <Card.Title>Results:</Card.Title>
+                    <Container className="col-lg-8">
+                        {items.map((item, index) => (
+                            <ListGroup key={index} className="my-2">
+                                <ListGroup.Item>{item}</ListGroup.Item>
+                            </ListGroup>
+                        ))}
+                    </Container>
+                </Card.Body>
+            </Card>
+        </Container>
+    );
+}
