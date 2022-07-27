ddafe7ce1ed56695fef2b3235e4d0157d62cadbb
haroldnc <harold.mth95@gmail.com>
2022-07-26 23:23:05 -0500

    Acoplamos el componente CardResult en App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
+import CardResult from "./components/CardResult";
 
 function App() {
+    console.log("render: App");
+
     return (
         <>
             <NavBar />
+            <CardResult />
         </>
     );
 }
